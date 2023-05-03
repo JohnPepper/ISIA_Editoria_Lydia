1bcc5f3151c1550dc79160105872f44758d04bf3
JohnPepper <flickerise@gmail.com>
2023-05-03 11:41:30 +0200

    horrible

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import time
 API_TOKEN = '5627308480:AAH0kfXZSvVdI1fb24Jc5v3v5Cec6rpaN98' #telegram token, no change
 OPENAI_API_KEY = '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' #OpenAI token, to change
 ser = serial.Serial('/dev/cu.SLAB_USBtoUART', 115200)
-
+jjj
 
 # Inizializza bot telegram
 bot = telebot.TeleBot(API_TOKEN)
