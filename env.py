21c1f59db1871e26eca2e9894371a05c0f3c745a
Neal <neal.norman@icloud.com>
2023-06-02 16:11:23 +0200

    envtest

--- env.py
+++ env.py
@@ -1,2 +1,2 @@
 bot_token = '6149818994:AAEu2QDUHTvgJ61SN3fENN7efxQP_neTzKI'
-API_KEY = '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'
+API_KEY = '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'ccxxc
