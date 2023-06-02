5cef0e81a912a8ca9b555034d97f9fb3ecc1035a
Neal <neal.norman@icloud.com>
2023-06-02 15:52:03 +0200

    removing noah

--- Noah/convert.py
+++
@@ -1,9 +0,0 @@
-from pydub import AudioSegment
-
-# AudioSegment.converter = "/john_pepper/audio-orchestrator-ffmpeg/bin"
-# AudioSegment.ffmpeg = "/john_pepper/audio-orchestrator-ffmpeg/bin"
-# AudioSegment.ffprobe ="/john_pepper/audio-orchestrator-ffmpeg/bin"
-
-def convert_to_mp3(file_path):
-    sound = AudioSegment.from_file(file_path, format="ogg")
-    sound.export(f"{file_path}.mp3", format="mp3", bitrate="128k")
--- Noah/env.py
+++
@@ -1,4 +0,0 @@
-API_TOKEN = '5627308480:AAH0kfXZSvVdI1fb24Jc5v3v5Cec6rpaN98'  # telegram token, no change
-OPENAI_API_KEY = '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'  # OpenAI token, to change
-
-#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
