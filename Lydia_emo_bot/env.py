97a02e19e315239d2f7fc33c74f71e9adac85a1f
Noah <noahlight98@icloud.com>
2023-06-20 11:15:47 +0200

    final code

--- Lydia_emo_bot/env.py
+++ Lydia_emo_bot/env.py
@@ -2,5 +2,5 @@
 #API_KEY = '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'
 
 bot_token = '6203148881:AAH4UVGRpuX7Rn5HsAkfm7wVFmF37V9F-_c'  # telegram token, no change
-API_KEY = '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'  # Noah OpenAI token, to change
-#API_KEY = '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'  # Reghina OpenAI token, to change
+#API_KEY = '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'  # Noah OpenAI token, to change
+API_KEY = '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'  # Reghina OpenAI token, to change
